53675eaaa19f2d2ea94c9476421a63a2cdf7e544
jugal prajapati <jugalprajapati381@gmail.com>
2026-03-29 11:56:48 +0530

    u

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function toggleNav() {
     if (menu.style.width === "250px") {
         menu.style.width = "0";
     } else {
-        menu.style.width = "250px";
+        menu.style.width = "0px";
     }
 }
 
